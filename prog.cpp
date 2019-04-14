75ffeb46e80b1179c97ba41ae593d8ef3d74a7cd
nipunsingal <500069052@stu.upes.ac.in>
2019-04-14 19:17:24 +0530

    file added

---
+++ prog.cpp
@@ -0,0 +1,144 @@
+#include<iostream>
+#include<conio.h>
+#include<fstream>
+
+using namespace std;
+
+//START OF CLASS
+
+
+
+class hotel
+{
+
+int room_no;
+char name[30];
+char address[50];
+char phone[10];
+
+public:
+
+void main_menu();		//to dispay the main menu
+void add();			//to book a room
+void display(); 		//to display the customer record
+void rooms();			//to display alloted rooms
+void edit();			//to edit the customer record
+int check(int);			//to check room status
+void modify(int);		//to modify the record
+void delete_rec(int);		//to delete the record
+void bill(int);                 //for the bill of a record
+     };
+    //END OF CLASS
+
+
+
+//FOR DISPLAYING MAIN MENU
+
+
+
+void hotel::main_menu()
+{
+
+int choice;
+while(choice!=5)
+{
+
+  system("cls");
+cout<<"\n\t\t\t\t*************************";
+cout<<"\n\t\t\t\t SIMPLE HOTEL MANAGEMENT ";
+cout<<"\n\t\t\t\t      * MAIN MENU *";
+cout<<"\n\t\t\t\t*************************";
+cout<<"\n\n\n\t\t\t1.Book A Room";
+cout<<"\n\t\t\t2.Customer Records";
+cout<<"\n\t\t\t3.Rooms Allotted";
+cout<<"\n\t\t\t4.Edit Record";
+cout<<"\n\t\t\t5.Exit";
+cout<<"\n\n\t\t\tEnter Your Choice: ";
+cin>>choice;
+
+switch(choice)
+{
+
+case 1:	add();
+break;
+
+case 2: display();
+break;
+
+case 3: rooms();
+break;
+
+case 4:	edit();
+break;
+
+case 5: break;
+
+default:
+{
+
+cout<<"\n\n\t\t\tWrong choice.....!!!";
+cout<<"\n\t\t\tPress any key to   continue....!!";
+getch();
+
+}
+
+}
+
+}
+
+}
+
+
+//END OF MENU FUNCTION
+
+
+//FUNCTION FOR BOOKING OF ROOM
+
+
+void hotel::add()
+{
+
+  system("cls");
+int r,flag;
+ofstream fout("Record.dat",ios::app);
+
+cout<<"\n Enter Customer Detalis";
+cout<<"\n ----------------------";
+cout<<"\n\n Room no: ";
+cout<<"\n Total no. of Rooms - 50";
+cout<<"\n Ordinary Rooms from 1 - 30";
+cout<<"\n Luxuary Rooms from 31 - 45";
+cout<<"\n Royal Rooms from 46 - 50";
+cout <<"\n Enter The Room no. you want to stay in :- "<<endl;
+cin>>r;
+
+flag=check(r);
+
+if(flag)
+cout<<"\n Sorry..!!!Room is already booked";
+
+else
+{
+
+room_no=r;
+cout<<" Name: ";
+cin>>name;
+cout<<" Address: ";
+cin>>address;
+cout<<" Phone No: ";
+cin>>phone;
+
+fout.write((char*)this,sizeof(hotel));
+cout<<"\n Room is booked...!!!";
+
+}
+
+cout<<"\n Press any key to continue.....!!";
+
+getch();
+fout.close();
+
+}
+
+
+//END OF BOOKING FUNCTION
